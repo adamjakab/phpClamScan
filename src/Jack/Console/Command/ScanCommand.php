819de229723323cbd3ac5f7a3781ef1a151323a2
jakabadambalazs <jakabadambalazs@gmail.com>
2015-05-08 12:29:03 +0200

    moving ahead

--- src/Jack/Console/Command/ScanCommand.php
+++ src/Jack/Console/Command/ScanCommand.php
@@ -13,6 +13,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
 
 class ScanCommand extends Command
 {
@@ -24,6 +26,17 @@ class ScanCommand extends Command
 
 	/** @var  OutputInterface */
 	private $output;
+
+	/** @var Filesystem */
+	private $fs;
+
+	/**
+	 * @param string $name
+	 */
+	public function __construct($name = null) {
+		parent::__construct($name);
+		$this->fs = new Filesystem();
+	}
 
 	/**
 	 * Configure command
@@ -49,90 +62,80 @@ class ScanCommand extends Command
 		$config_file = $input->getArgument('config_file');
 		$this->parseConfiguration($config_file);
 
-		if(isset($this->config["paths"]) && is_array($this->config["paths"]) && count($this->config["paths"])) {
-			foreach($this->config["paths"] as $path) {
-				$this->output->writeln("Scanning folder: <info>".$path."</info>");
-				$this->scanDirectory($path);
-			}
-			$this->sendInfectionReport();
+		$listFilePath = $this->buildFileList();
+		$this->scan($listFilePath);
+		//$this->sendInfectionReport();
+	}
+
+
+	/**
+	 *
+	 * /usr/bin/clamscan -d /usr/local/maldetect/tmp/.runtime.user.21512.hdb -d /usr/local/maldetect/tmp/.runtime.user.21512.ndb  -r --infected --no-summary -f /usr/local/maldetect/tmp/.find.21512
+	 *
+	 * @param string $listFilePath
+	 */
+	private function scan($listFilePath) {
+		if(!$listFilePath) {
+			$this->output->writeln("No files to scan!");
+			return;
+		}
+
+		//Default Virus Scan
+		$CMD = $this->config["clamscan_binary"]
+		       . " --infected"
+		       . " --no-summary"
+		       . " -f " . $listFilePath
+			;
+		$this->output->writeln("Scanning($CMD)...");
+		exec($CMD, $SCANRES_1, $RV);
+		if($RV != 0 && ($viruscount = count($SCANRES_1)) ) {
+			$this->handleInfectedfiles($SCANRES_1);
 		} else {
-			$this->output->writeln("<error>No usable scan paths were found!</error>");
-			return false;
+			$this->output->writeln("Clean");
 		}
-		return true;
+
+		//Optional additional database scans
+
+
+		$this->fs->remove($listFilePath);
 	}
 
-	/**
-	 * @param string $config_file
-	 */
-	private function parseConfiguration($config_file) {
-		if(!file_exists($config_file)) {
-			throw new \InvalidArgumentException("The configuration file does not exist!");
-		}
 
-		$yamlParser = new Parser();
-		$config = $yamlParser->parse(file_get_contents($config_file));
-		if(!is_array($config) || !isset($config["config"])) {
-			throw new \InvalidArgumentException("Malformed configuration file!");
-		}
-		$this->config = $config["config"];
-
-		//check and create quarantene folder
-		if(!$this->_checkDir($this->config["quarantente_path"])) {
-			mkdir($this->config["quarantente_path"], 0700, true);
-		}
-	}
-
-	/**
-	 * @param string $dir
-	 */
-	private function scanDirectory($dir) {
-		if(!$this->_checkDir($dir)) {
-			$this->output->writeln("Directory($dir) does not exist!");
+	private function handleInfectedfiles($scanResults) {
+		if(is_array($scanResults) && ($viruscount = count($scanResults))) {
+			$this->output->writeln("Found viruses: " . $viruscount);
+		} else {
 			return;
 		}
 
-		$checkList = glob($dir."/*");
-		if(!count($checkList)) {
-			return;
-		}
-
-		foreach($checkList as $checkFile) {
-			if(is_dir($checkFile)) {
-				$this->scanDirectory($checkFile);
+		foreach($scanResults as &$scanResult) {
+			$scanResult = $this->elaborateScanResult($scanResult);
+			$scanResult["action"] = strtoupper($this->config["infection_action"]);
+			if(!$scanResult["file"]) {
+				$scanResult["result"] = "File has disappeared!";
 			} else {
-				$this->scanFile($checkFile);
+				switch($scanResult["action"]) {
+					case "NONE":
+						$scanResult["result"] = "No action taken.";
+						break;
+					case "QUARANTENE":
+						$scanResult["result"] = "Moved to quarantene.";
+						break;
+					case "DELETE":
+						$scanResult["result"] = "Deleted.";
+						break;
+					default:
+						$scanResult["result"] = "Unknown action! No action taken.";
+						break;
+				}
 			}
 		}
-	}
+		print_r($scanResults);
 
-	/**
-	 * @param string $file
-	 */
-	private function scanFile($file) {
-		if(!$this->_checkFile($file)) {
-			$this->output->writeln("File($file) does not exist!");
-			return;
-		}
+		$this->output->writeln("Executed action on all infected files: " . strtoupper($this->config["infection_action"]));
 
-		if(($fileSize=filesize($file)) > $this->config["max_file_size"]) {
-			return;
-		}
 
-		$CMD = 'cat "' . $file . '" | ' . $this->config["clamscan_binary"] . " " . $this->config["clamscan_options"] . ' -';
-		exec($CMD, $RES, $RV);
-		if($RV != 0) {
-			$this->handleInfectedfile($file, $RES);
-		}
-	}
-
-	private function handleInfectedfile($file, $scanResult) {
-		$infectionName = "Unknown";
-		if(is_array($scanResult)&&isset($scanResult[0])) {
-			$infectionName = trim(str_replace(["stream:","FOUND"],["",""],$scanResult[0]));
-
-		}
-
+		/*
 		$this->output->writeln("File($file): <error>Infected[$infectionName]</error>"
 		                       .($this->config["move_to_quarantene"] ? " - Quarantened" : ""));
 
@@ -149,12 +152,27 @@ class ScanCommand extends Command
 				$this->output->writeln("Unable to move infected file to: " . $quarantenePath);
 				return;
 			}
-
 			//chown($quarantenePath, "root");
 			//chgrp($quarantenePath, "root");
 			//chmod($quarantenePath, 0);
 		}
+		*/
 	}
+
+	/**
+	 * @param string $scanResult
+	 * @return array
+	 */
+	private function elaborateScanResult($scanResult) {
+		$answer = ["file"=>null, "infection"=>null];
+		$tmp = explode(":", $scanResult);
+		$answer["infection"] = trim(str_replace("FOUND","",array_pop($tmp)));
+		$answer["file"] = realpath(trim(implode((count($tmp)>1?":":""), $tmp)));
+		return $answer;
+	}
+
+
+
 
 	private function sendInfectionReport() {
 		if(!$this->config["report"]["enabled"] || !count($this->infections)) {
@@ -180,6 +198,67 @@ class ScanCommand extends Command
 		$mailer->send($msg);
 	}
 
+	/**
+	 * @return bool|string
+	 */
+	private function buildFileList() {
+		if(isset($this->config["paths"]) && is_array($this->config["paths"]) && count($this->config["paths"])) {
+			$fileName = md5("FileList-".microtime()).'.txt';
+			$filePath = $this->config["temporary_path"] . $fileName;
+			$filesList  = [];
+			foreach($this->config["paths"] as $path) {
+				$filesList = array_merge($filesList, $this->parseFolder($path));
+			}
+			$this->fs->dumpFile($filePath, implode("\n", $filesList));
+			$this->output->writeln("Files to scan: " . count($filesList));
+			return $filePath;
+		} else {
+			$this->output->writeln("<error>No usable scan paths were found!</error>");
+			return false;
+		}
+	}
+
+	/**
+	 * @param string $dir
+	 * @return array
+	 */
+	private function parseFolder($dir) {
+		$list = [];
+		if($this->_checkDir($dir)) {
+			$checkList = glob($dir."/*");
+			if(count($checkList)) {
+				foreach ($checkList as $checkFile) {
+					if (is_dir($checkFile)) {
+						$list = array_merge($list, $this->parseFolder($checkFile));
+					} else {
+						$list[] = realpath($checkFile);
+					}
+				}
+			}
+		}
+		return $list;
+	}
+
+	/**
+	 * @param string $config_file
+	 */
+	private function parseConfiguration($config_file) {
+		if(!file_exists($config_file)) {
+			throw new \InvalidArgumentException("The configuration file does not exist!");
+		}
+
+		$yamlParser = new Parser();
+		$config = $yamlParser->parse(file_get_contents($config_file));
+		if(!is_array($config) || !isset($config["config"])) {
+			throw new \InvalidArgumentException("Malformed configuration file!");
+		}
+		$this->config = $config["config"];
+
+		//check and create quarantene folder
+		if(!$this->_checkDir($this->config["quarantente_path"])) {
+			mkdir($this->config["quarantente_path"], 0700, true);
+		}
+	}
 
 	private function _checkFile($file) {
 		return(file_exists($file) && is_file($file));
